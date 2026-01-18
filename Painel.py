aec95b765ae8b57335c95a8a622612ddd596cf7e
vitoriapguimaraes <vpistoriguimaraes@gmail.com>
2026-01-18 16:32:33 -0300

    (refactor) Home to PT

--- Painel.py
+++ Painel.py
@@ -7,12 +7,13 @@ st.set_page_config(page_title="Análise de Dados e BI", page_icon="📊", layout
 st.title("Análise de Dados e Business Intelligence")
 
 st.code(
-    "Acesse a AED do dataset, seguida pela análise e visualizações, na lista abaixo ou na barra lateral"
+    "Acesse a Análise Exploratória de Dados, seguida pela Visualizações, Métricas e Insights, na lista abaixo ou na barra lateral"
 )
 
 st.page_link(
     "pages/1-Analise_de_Cancelamento_de_Clientes.py",
     label="Análise de Cancelamento de Cartão de Crédito",
+    use_container_width=True,
 )
 
 st.markdown("---")
