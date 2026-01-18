1210780819b4a221a593bb3c25339b761e147669
vitoriapguimaraes <vpistoriguimaraes@gmail.com>
2026-01-18 15:15:25 -0300

    (feature) crete home and first page

---
+++ utils/load_file.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+from utils.paths import DATA_DIR
+
+
+@st.cache_data
+def load_dataset(file_name):
+    """
+    Load a CSV file from the data directory.
+    Uses centralized path management from utils.paths.
+    """
+    file_path = DATA_DIR / file_name
+    try:
+        return pd.read_csv(file_path, encoding="utf-8")
+    except UnicodeDecodeError:
+        return pd.read_csv(file_path, encoding="latin-1")
